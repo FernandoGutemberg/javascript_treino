2031e20cceab4b684c2076ae0f87c43d21160716
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-21 12:21:07 -0300

    Sobre logica

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,10 @@
-console.log( 'O console funcionou corretamente')
+//console.log( 'O console funcionou corretamente')
+//O recul, espaçamento é o nosso chamado IDENTAÇAO
+
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //um condição simples, porque não tem Else
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+//Condicional composta
+var pais = 'Argentina'
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
+
+// var pais = 'Argentina'
+// if (pais != 'Brasil') {
+//     console.log('Brasileiro')
+// } else {
+//     console.log('Estrangeiro')
+// }
