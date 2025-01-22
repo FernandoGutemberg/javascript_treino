6e628214abbe2533ce0927ded371f5e6d2bf532c
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-22 15:54:42 -0300

    Entendo o switch na pratica

---
+++ aula11/ex011pt2.js
@@ -0,0 +1,11 @@
+var idade = 19
+console.log(`Sua idade é ${idade}`)
+
+if (idade > 18) {
+    console.log("Você pode tirar a carteira no Brasil")
+} else if (idade < 18) {
+    console.log("Você ainda não pode tirar CNH, porque a legislação não permite")
+
+} else {
+
+}
