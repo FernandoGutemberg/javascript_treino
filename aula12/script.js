bfbc00ca57e43ee9eee02f2aead96f624e7f9809
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-29 12:18:55 -0300

    Exercicio de horario

--- aula12/script.js
+++ aula12/script.js
@@ -1,1 +1,26 @@
-alert("Olá")
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora =  data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //console.log("Bom dia!");
+        img.src = 'manha.png'
+        document.body.style.background = '#3e3a27'
+    } else if (hora >= 12 && hora < 18) {
+        //console.log("Boa Tarde!");
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#482713'
+
+
+    } else {
+        //console.log("Boa Noite");
+        img.src = 'noite.png'
+        document.body.style.background = '#17314a'
+
+
+    }
+
+}
